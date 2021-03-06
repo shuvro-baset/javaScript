04c4f500f7d611fc317d65aaad4a6bb40480cd04
shuvro baset <md.abdul.baset75@gmail.com>
2021-03-07 04:54:17 +0600

    before function start

--- js_basic/array.js
+++ js_basic/array.js
@@ -11,13 +11,24 @@ console.log(a[0])
 // adding new element. this time it will add an element in the last position of the element.
 a.push(13)
 console.log(a)
+// adding new element as a 1st element.
+a.unshift(100)
 
-a.insert(5, 5)
+// adding new value in specific position (start, delete item, value)
+a.splice(6,0,37)
 console.log(a)
+
 
 // delete last element from the array
 a.pop()
 console.log(a)
-
+//delete 1st element from the array
+a.shift()
 // array length
 console.log(a.length)
+
+// ---- slicing array ---- // 
+var sliceArray = ['baset', 'shuvro', 23, 75];
+// slice function take start index and last index
+console.log(sliceArray.slice(2))
+console.log(sliceArray.slice(0,2 ))
---
+++ js_basic/loop.js
@@ -0,0 +1,16 @@
+var a = 1;
+// while loop . 
+while(a<4){
+    console.log(a);
+    a++
+}
+
+// for loop 
+for(var i=0; i<10; i++){
+    console.log(i)
+}
+
+var list_item = ['baset', 'shuvro', 23, 75];
+for(var i=0; i<list_item.length; i++){
+    console.log(list_item[i])
+}
