a60986917385f3625add527636ae5c712ce60e28
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-28 06:39:37 +0600

    comments done

--- api/my-practice/api.js
+++ api/my-practice/api.js
@@ -1,3 +1,18 @@
-fetch('https://jsonplaceholder.typicode.com/comments')
+function loadComments(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
-    .then(data => console.log(data[0].body));
+    .then(data => commentsDetails(data))
+}
+// direct function calling if I need all the data when page is loaded. no onclick button.
+loadComments();
+function commentsDetails(comments){
+    const commentsDiv = document.getElementById('comments');
+    for(const d of comments){
+        const h4 =document.createElement('h4');
+        const p = document.createElement('p');
+        h4.innerText = d.name;
+        p.innerText = d.body;
+        commentsDiv.appendChild(h4);
+        commentsDiv.appendChild(p);
+    }
+}
