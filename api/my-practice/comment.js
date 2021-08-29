416ac9ffaa1e258c2d4b62739d29bd5388fd37bb
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-29 06:54:32 +0600

    user part done

--- api/my-practice/comment.js
+++ api/my-practice/comment.js
@@ -1,6 +1,12 @@
+/*
+    load comments from jsonPlaceholder api comments data source. here I am trying onload data and also after clicked. 
+    another feature is when user click a comment button then it will show the details about that specific comment.
+*/
+// loadComment function is for load the data 
 const loadComments = () => {
     fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
+    // calling the commentDetail function will show in the html page.
     .then(data => commentsDetails(data))
 }
 // direct function calling if I need all the data when page is loaded. no onclick button.
@@ -8,6 +14,7 @@ loadComments();
 
 
 const commentsDetails = comments => {
+    // taking parent div for showing comments details
     const commentsDiv = document.getElementById('comments');
     comments.forEach(comment => {
         const div = document.createElement('div');
@@ -20,7 +27,7 @@ const commentsDetails = comments => {
         commentsDiv.appendChild(div);
     });
 }
-
+// specific commentsDetails function
 const loadCommentsById = id => {
     const url = `https://jsonplaceholder.typicode.com/comments/${id}`;
     fetch(url)
@@ -28,7 +35,7 @@ const loadCommentsById = id => {
         .then(data => displayCommentDetail(data));
     
 }
-
+// showing specific comment details dynamically.
 const displayCommentDetail = comment => {
     console.log(comment);
     const commentDiv = document.getElementById('comment-details');
