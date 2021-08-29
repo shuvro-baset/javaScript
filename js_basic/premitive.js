f39c78f793e438b2ea589032ccabb7b6bc63bdf5
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-30 04:24:08 +0600

    adsf

---
+++ js_basic/premitive.js
@@ -0,0 +1,32 @@
+ /*
+
+    ---- Primitive data type / immutable
+    1. number 
+    2. string
+    3. boolean
+    4. undefined 
+    5. null 
+    6. symbol
+    ----- non primitive data type  / mutable
+    7. object 
+
+
+    truthy          |   falsy
+    true            |    false
+    number(+/-)     |     0
+    any string      |   empty string
+                    |   null, NaN
+                    |     undefined
+ 
+    * if primitive type then I should compare with double equal
+    * if object type then I should compare with triple equal             
+                 
+*/
+
+function sum(p,q) {
+
+    console.log(p+q);
+    
+    }
+
+const result=sum(2,3);
